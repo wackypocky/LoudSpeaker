76a5ca211b4312fc0a06b4909eceffa53f3a1032
Annie Zhou <anniez@princeton.edu>
2019-07-22 11:24:33 -0400

    working version 2 of merge

--- merge.py
+++ merge.py
@@ -23,48 +23,43 @@ def merge(source_path: str, dest_path: str):
   fnames: List[str] = sorted_ls(source_path)
   if not os.path.exists(dest_path):
     os.mkdir(dest_path)
-  print(fnames)
   while fnames:
-    first:str = fnames[0]
+    first:str = fnames.pop(0)
     if not first.lower().endswith(('.wav', '.m4a', '.mp3', 'mp4')):
-      fnames.pop(0)
       continue
     absfirst = os.path.abspath(os.path.join(source_path, first))
     fdur:float = duration(absfirst) # in seconds
     # If file is already > 5min = 300sec, don't touch
     if fdur >= 300.0:
       shutil.copy(absfirst, dest_path)
-      fnames.pop(0)
       continue
 
     # Else file is < 5 minutes, combine with others
     totaldur:float = 0.0
-    absnext = absfirst
+    last = ''
     inlistname:str = os.path.join(source_path, 'inlist.txt')
     input = open(inlistname, 'a')
-    #print('file', "'%s'" %(absfirst), file=input)
-    #input: List[str] = ['-i', absfirst] # list of all input arguments
+    print('file', "'%s'" %(absfirst), file=input)
+
     # If the next file + the current file > 10 min = 600sec, don't touch
     while fnames and totaldur + fdur <= 600.0:
-      print(fnames.pop(0)) # pop the current last item in merge (the one that corresponds with nextdur)
-      print('file', "'%s'" %(absnext), file=input)
-      last:str = absnext
-      # if nextdur:   # if not the first iteration
-      #   print('file', "'%s'" %(absnext), file=input)
-      #   last:str = next
-      if not fnames:
-        break
-      if not fnames[0].lower().endswith(('.wav', '.m4a', '.mp3', 'mp4')):
+      if totaldur:   # if not the first iteration
         fnames.pop(0)
-        continue
+        print('file', "'%s'" %(absnext), file=input)
+        last:str = next
+        if not fnames:
+          break
+        if not fnames[0].lower().endswith(('.wav', '.m4a', '.mp3', 'mp4')):
+          fnames.pop(0)
+          continue
       totaldur += fdur
       next:str = fnames[0]
       absnext = os.path.abspath(os.path.join(source_path, next))
-      fdur: float = duration(absnext) 
+      fdur: float = duration(absnext)
 
     # either list is empty or next is the first in the next combined file
     # so concat
-    if last == absfirst:  # if no files concat with first file, then just copy first
+    if not last:  # if no files concat with first file, then just copy first
       shutil.copy(absfirst, dest_path)
       continue
     match = re.search('([\w-]+).wav', absfirst)
@@ -84,7 +79,7 @@ def merge(source_path: str, dest_path: str):
                        '-c', 'copy', destfilename]
     p = subprocess.Popen(args)
     p.wait()
-    #p = subprocess.run(['rm', inlistname])
+    p = subprocess.run(['rm', inlistname])
 
 # iterate through the directories given as command line arguments
 def main():
