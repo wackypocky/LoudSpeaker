cf4787c0b73c87dfe486d7f804d7eb4be59596f4
Annie Zhou <pianistanniez@gmail.com>
2019-08-06 12:12:10 -0400

    reorganized a bit + added stats_client.py

--- stats.py
+++ stats.py
@@ -12,9 +12,9 @@ from pydub import AudioSegment
 import IPython
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
-def iterate_dirs(source_dir:str, data_tuple = None, non_speech_dur:int = 0,
-                 speech_dur:int = 0):
+def iterate_dirs(source_dir:str, data_tuple = None, speech_dur:int = 0):
   if data_tuple is None:
     audio_by_class = [0] * 10
     baby_hours = [0] * 24
@@ -40,12 +40,11 @@ def iterate_dirs(source_dir:str, data_tuple = None, non_speech_dur:int = 0,
       this_clip = AudioSegment.from_wav(clip_path)
       duration_ms:int = len(this_clip)
 
-      # update total non_speech duration if class is 'noise'
+      # do nothing if class is 'noise'
       if dir_name == 'noise':
-        non_speech_dur += duration_ms
         continue
 
-      # otherwise, update the other stats
+      # otherwise, update stats
       speech_dur += duration_ms
       class_nums:List[str] = dir_name.split(',')
       # update class stats
@@ -60,21 +59,34 @@ def iterate_dirs(source_dir:str, data_tuple = None, non_speech_dur:int = 0,
           sys.exit(1)
         time_hour:int = int(match.group(1))
         baby_hours[time_hour] += duration_ms
-  return (audio_by_class, baby_hours, non_speech_dur, speech_dur)
+  return (audio_by_class[1:], baby_hours, speech_dur)
 
 # Create a pie chart w/ given data (list of ints)
 def pie(name:str, dest_dir:str, data:List[int], labels:List[str]):
-  pie_path:str = os.path.join(dest_dir, name)
+  full_name:str = name + '.png'
+  pie_path:str = os.path.join(dest_dir, full_name)
   plt.pie(data, labels=labels)
+  plt.title(name)
   plt.savefig(pie_path)
   plt.clf()
 
-# # Create a bar graph w/ given data (list of ints)
-# def bar(data:List[int]):
+# Create a bar graph w/ given data (list of ints)
+def bar(name:str, dest_dir:str, data:List[int], labels:List[str], x_label:str):
+  full_name:str = name + '.png'
+  bar_path:str = os.path.join(dest_dir, full_name)
+  y_pos = np.arange(len(labels))
+  plt.bar(y_pos, data, align='center')
+  plt.xticks(y_pos, labels)
+  plt.xlabel(x_label)
+  plt.ylabel('Duration (ms)')
+  plt.title(name)
+  plt.savefig(bar_path)
+  plt.clf()
 
 # Create an excel file w/ given data (list of ints)
 def to_excel(xl_name:str, dest_dir:str, data:List[int], labels:List[str] = []):
-  xl_path:str = os.path.join(dest_dir, xl_name)
+  xl_full_name = xl_name + '.xlsx'
+  xl_path:str = os.path.join(dest_dir, xl_full_name)
   data_in_sec = [time / 1000 for time in data]
   if labels:
     df = pd.DataFrame(data=data_in_sec, index=labels, columns=['Duration (sec)'])
@@ -82,17 +94,17 @@ def to_excel(xl_name:str, dest_dir:str, data:List[int], labels:List[str] = []):
     df = pd.DataFrame(data=data_in_sec, columns=['Duration (sec)'])
   df.to_excel(xl_path)
 
+# optional usage of stats over multiple directories
 def main():
   args = sys.argv[1:]
   if not args:
-    print('usage: stats.py {--by_dir | --total} file');
+    print('usage: stats.py {--by_dir | --total} dir');
     sys.exit(1)
 
   option = args[0]
   dirs = args[1:]
   classes:List[str] = ['Male', 'Female', 'Male_paren', 'Female_paren',
                        'Babble_marg', 'Babble_dup', 'Babble_var', 'Baby_coo', 'Baby_cry']
-  labels:List[str] = ['Speech (adult/baby)', 'Non-speech (silence/noise)']
 
   # iterate through input directories
   for dirname in dirs:
@@ -100,27 +112,23 @@ def main():
     if not entries:  # if the directory is empty, continue
       continue
     if option == '--by_dir':
-      (audio_by_class, baby_hours, non_speech_dur, speech_dur) = iterate_dirs(dirname)
-      audio_by_class = audio_by_class[1:] # don't include first cell
+      (audio_by_class, baby_hours, speech_dur) = iterate_dirs(dirname)
+      print(dirname, ":", speech_dur)
       to_excel('audio_by_class.xlsx', dirname, audio_by_class, classes)
       to_excel('baby_hours.xlsx', dirname, baby_hours)
-      speech_vs_noise:List[int] = [speech_dur, non_speech_dur]
-      to_excel('speech_vs_noise.xlsx', dirname, speech_vs_noise, labels)
       pie('audio_by_class.png', dirname, audio_by_class, classes)
     else:
       if dirname == dirs[0]:
-        (audio_by_class, baby_hours, non_speech_dur, speech_dur) = iterate_dirs(dirname)
+        (audio_by_class, baby_hours, speech_dur) = iterate_dirs(dirname)
       else:
-        (audio_by_class, baby_hours, non_speech_dur, speech_dur) = iterate_dirs(dirname, (audio_by_class, baby_hours), non_speech_dur, speech_dur)
+        (audio_by_class, baby_hours, speech_dur) = iterate_dirs(dirname, (audio_by_class, baby_hours), speech_dur)
 
   # Create total stats over all directories if option is --total in cwd
   if option == '--total':
     cwd = os.getcwd()
-    audio_by_class = audio_by_class[1:] # don't include first cell
+    print(dirname, ":", speech_dur)
     to_excel('audio_by_class.xlsx', cwd, audio_by_class, classes)
     to_excel('baby_hours.xlsx', cwd, baby_hours)
-    speech_vs_noise:List[int] = [speech_dur, non_speech_dur]
-    to_excel('speech_vs_noise.xlsx', cwd, speech_vs_noise, labels)
     pie('audio_by_class.png', cwd, audio_by_class, classes)
 
 if __name__ == '__main__':
---
+++ stats_client.py
@@ -0,0 +1,67 @@
+import os
+import sys
+from typing import List, Dict
+
+import pydub
+from pydub import AudioSegment
+import IPython
+import stats
+
+def duration_dir(source_path:str):
+  total_dur = 0
+  fnames:List[str] = os.listdir(source_path)
+  for fname in fnames:
+    if not fname.lower().endswith(('.wav')):
+      continue
+    clip_path:str = os.path.join(source_path, fname)
+    this_clip = AudioSegment.from_wav(clip_path)
+    duration_ms:int = len(this_clip)
+    total_dur += duration_ms
+  return total_dur
+
+def main():
+  args = sys.argv[1:]
+  if not args:
+    print('usage: stats_client.py final_dir raw_dirs');
+    sys.exit(1)
+
+  final_dir = args[0]
+  raw_dirs = args[1:]
+
+  # iterate through raw directories OR take the existing duration
+  cwd = os.getcwd()
+  path_to_file = os.path.join(cwd, 'total_dur_raw.txt')
+  if os.path.exists(path_to_file):
+    with open(path_to_file) as f:
+      initial_dur:int = int(f.read())
+  else:
+    initial_dur:int = 0
+    for dirname in raw_dirs:
+      initial_dur += duration_dir(dirname)
+    f = open(path_to_file), 'w')
+    f.write(initial_dur)
+    f.close()
+
+  # grab stats using final_dir as input
+  (audio_by_class, baby_hours, speech_dur) = stats.iterate_dirs(final_dir)
+  non_speech_dur:int = initial_dur - speech_dur
+  speech_vs_noise:List[int] = [speech_dur, non_speech_dur]
+
+  # export stats to excel files
+  labels:List[str] = ['Speech (adult/baby)', 'Non-speech (silence/noise)']
+  classes:List[str] = ['Male', 'Female', 'Male_paren', 'Female_paren',
+                       'Babble_marg', 'Babble_dup', 'Babble_var', 'Baby_coo',
+                       'Baby_cry']
+  hours:List[str] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                     '11', '12', '13', '14', '15', '16', '17', '18', '19',
+                     '20', '21', '22', '23']
+  cwd = os.getcwd()
+  stats.to_excel('speech_vs_noise', cwd, speech_vs_noise, labels)
+  stats.to_excel('audio_by_class', cwd, audio_by_class, classes)
+  stats.to_excel('baby_hours', cwd, baby_hours)
+  stats.pie('audio_by_class', cwd, audio_by_class, classes)
+  stats.pie('speech_vs_noise', cwd, speech_vs_noise, labels)
+  stats.bar('baby_hours', cwd, baby_hours, hours, 'Hour (24-hour clock)')
+
+if __name__ == '__main__':
+  main()
